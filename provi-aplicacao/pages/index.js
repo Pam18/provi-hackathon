27a8fdc66e700054f9885610a59a2d190b9588da
Yhaohannah Lima <yha.lima@outlook.com>
2021-11-28 04:47:12 -0300

    feat: criação da seção de depoimentos na página inicial

--- provi-aplicacao/pages/index.js
+++ provi-aplicacao/pages/index.js
@@ -20,6 +20,12 @@ import rede from "../public/assets/rede.svg";
 import cadastro from "../public/assets/cadastro.svg";
 import seta from "../public/assets/seta.svg";
 
+import bandeiraTrans from "../public/assets/bandeira-trans.svg";
+import pessoaTrans from "../public/assets/pessoa-trans.svg";
+import aspas from "../public/assets/aspas.svg";
+import setaEsquerda from "../public/assets/seta-cinza-esquerda.svg";
+import setaDireita from "../public/assets/seta-preta-direita.svg";
+
 
 export default function Home() {
   return (
@@ -128,6 +134,59 @@ export default function Home() {
               <Image src={seta} alt="seta verde apontando para a direita" />
             </a>
           </div>
+        </div>
+      </div>
+
+      <div id="depoimentos" className={styles.depoimentos}>
+        <div>
+          <span className={styles.bandeiraTrans}>
+            <Image
+              src={bandeiraTrans}
+              alt="imagem de uma mão levantando a bandeira trans, nas cores azul, rosa e branco"
+              width={500}
+            />
+          </span>
+
+          <span className={styles.pessoaTrans}>
+            <Image
+              src={pessoaTrans}
+              alt="imagem de uma mulher"
+              width={480}
+            />
+          </span>
+        </div>
+
+        <div className={styles.depo}>
+          <h2>Não acreditei que nós podemos usar nosso nome legalmente</h2>
+
+          <div className={styles.imagemDepo}>
+            <span className={styles.aspas}>"</span>
+
+            <div>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters. ”
+              </p>
+
+              <div className={styles.rodapeDepo}>
+                <div className={styles.infoDepo}>
+                  <span className={styles.nome}>Gabriela Sampaio</span>
+                  <span className={styles.periodo}>Usuária desde 2021</span>
+                </div>
+
+                <div className={styles.passarDepo}>
+                  <a href="#">
+                    <Image src={setaEsquerda} alt="imagem de uma mão levantando a bandeira trans, nas cores azul, rosa e branco" />
+                  </a>
+
+                  <a href="#">
+                    <Image src={setaDireita} alt="imagem de uma mão levantando a bandeira trans, nas cores azul, rosa e branco" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
